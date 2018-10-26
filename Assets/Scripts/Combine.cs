4f952daa8d0a7dc12051e44960fe4352abe8059e
LikeFrogs <davissnider42@hotmail.com>
2018-10-26 16:15:47 -0400

    world switching implemented

--- Assets/Scripts/Combine.cs
+++ Assets/Scripts/Combine.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Characters { World1, World2, Both }
+public enum Characters { World1, World2, Both, SuperTestPosition }
 
+/// <summary>
+/// Handles the combining and uncombining of the player characters
+/// based on user input
+/// </summary>
 public class Combine : MonoBehaviour
 {
     private Characters activeCharacters;
@@ -19,35 +23,58 @@ public class Combine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha1) && activeCharacters == Characters.Both)
-        {
-            activeCharacters = Characters.World1;
-
-            world2Character.transform.position = world1Character.transform.position;
-            world2Character.SetActive(false);
-        }
-        else if(Input.GetKeyDown(KeyCode.Alpha2) && activeCharacters == Characters.Both)
-        {
-            activeCharacters = Characters.World2;
-
-            world1Character.transform.position = world2Character.transform.position;
-            world1Character.SetActive(false);
-        }
-        else if(Input.GetKeyDown(KeyCode.Alpha3) && activeCharacters != Characters.Both)
+        //reactive the inactive character at the position of the character that is
+        //currently active
+        if (Input.GetKeyDown(KeyCode.Alpha3) && activeCharacters != Characters.Both)
         {
             activeCharacters = Characters.Both;
 
-            if(world1Character.activeInHierarchy)
+            if (world1Character.activeInHierarchy)
             {
                 world2Character.SetActive(true);
                 world2Character.transform.position = world1Character.transform.position;
-                world2Character.transform.rotation = world1Character.transform.rotation;
+                world2Character.transform.rotation = world1Character.transform.rotation;                
             }
             else
             {
                 world1Character.SetActive(true);
                 world1Character.transform.position = world2Character.transform.position;
                 world1Character.transform.rotation = world2Character.transform.rotation;
+            }
+
+            world1Character.layer = 9;
+            world2Character.layer = 10;
+        }
+
+        //combine ("measure out") into world1Character
+        if (Input.GetKeyDown(KeyCode.Alpha1) && activeCharacters == Characters.Both)
+        {
+            activeCharacters = Characters.World1;
+
+            world2Character.SetActive(false);
+
+            world1Character.layer = 9;
+        }
+        //combine ("measure out") into world2Character
+        else if(Input.GetKeyDown(KeyCode.Alpha2) && activeCharacters == Characters.Both)
+        {
+            activeCharacters = Characters.World2;
+            
+            world1Character.SetActive(false);
+
+            world2Character.layer = 10;
+        }
+        
+        //when only one character is active, pressing 4 causes that character to collide with all objects
+        if(Input.GetKeyDown(KeyCode.Alpha4) &&activeCharacters != Characters.Both && activeCharacters != Characters.SuperTestPosition)
+        {
+            if (world1Character.activeInHierarchy)
+            {
+                world1Character.layer = 11;
+            }
+            else
+            {
+                world2Character.layer = 11;
             }
         }
     }
