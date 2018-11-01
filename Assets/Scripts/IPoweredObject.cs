391b2203a1329f694ae2071fe35a27b21cae3dca
Justin Dennis <justindennis48@gmail.com>
2018-11-01 17:05:58 -0400

    Added powered object interface

---
+++ Assets/Scripts/IPoweredObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPoweredObject {
+
+    /// <summary>
+    /// Does whatever the object should do when it recieves power.
+    /// </summary>
+    void Activate();
+
+    /// <summary>
+    /// Removes power from the object.
+    /// </summary>
+    void Deactivate();
+}
