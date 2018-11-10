d21b06d0341713197c6295ce1dca4ee3793bd27b
Dane Sherman (RIT Student) <dms7827@ad.rit.edu>
2018-11-10 16:22:08 -0500

    UI features added

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    /// <summary>
+    /// controls the pause menu
+    /// </summary>
+
+    protected bool paused = false;
+
+    [SerializeField] private RectTransform topSection;
+    [SerializeField] private RectTransform bottomSection;
+
+	// Use this for initialization
+	void Start () {
+        topSection.position += new Vector3(-250f, 0);
+        bottomSection.position += new Vector3(0, -Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //triggers for toggling pause
+        bool pauseInput = Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape);
+
+        if (pauseInput)
+        {
+            paused = !paused;
+            if (paused)
+            {
+                topSection.localPosition = Vector2.zero;
+            }
+        }
+	}
+}
