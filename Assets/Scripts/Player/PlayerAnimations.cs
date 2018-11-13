7635980ddd666a6430ab3744fcd09504ba1fd6a6
Justin Dennis <jxd8037@g.rit.edu>
2018-11-13 17:32:30 -0500

    added player models and animations I made a new animated prefab because I didn't want to accidentally remove something on the old prefab and not add it to the new one, so far everything seems to be fine but I don't want to delete the old prefab until the new one has been tested.

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -4,13 +4,84 @@ using UnityEngine;
 
 public class PlayerAnimations : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private PlayerMovement movementController;
+    private Animator animator;
+    private Rigidbody rb;
+    private bool grounded;
+    [SerializeField]
+    private float defaultMoveSpeed, deadZone;
+
+    /// <summary>
+    /// The current move speed of the player in the XZ plane
+    /// </summary>
+    public float MoveSpeed
+    {
+        get { return animator.GetFloat("MoveVelocity"); }
+        set { animator.SetFloat("MoveVelocity", value); }
+    }
+
+    /// <summary>
+    /// The amount to multiply the move animation speed in the animator
+    /// </summary>
+    public float MoveAnimationMultiplier
+    {
+        get { return animator.GetFloat("MoveSpeedMultiplier"); }
+        set { animator.SetFloat("MoveSpeedMultiplier", value); }
+    }
+
+    /// <summary>
+    /// The current velocity of the player in the y direction
+    /// </summary>
+    public float JumpVelocity
+    {
+        get { return animator.GetFloat("JumpVelocity"); }
+        set { animator.SetFloat("JumpVelocity", value); }
+    }
+
+    /// <summary>
+    /// Whether or not the player is grounded calls the corresponding triggers
+    /// in the animator for when the player jumps or lands
+    /// </summary>
+    public bool Grounded
+    {
+        set
+        {
+            if(value != grounded)
+            {
+                if (value)
+                {
+                    animator.SetTrigger("Land");
+                }
+                else
+                {
+                    animator.SetTrigger("Jump");
+                }
+
+                grounded = value;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        movementController = GetComponent<PlayerMovement>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Grounded = movementController.Grounded;
+
+        MoveSpeed = movementController.Velocity2D.magnitude;
+
+        MoveAnimationMultiplier = MoveSpeed / defaultMoveSpeed;
+
+        JumpVelocity = rb.velocity.y;
+
+        if(MoveSpeed > deadZone)
+        {
+            transform.rotation = Quaternion.Euler(0, Mathf.Atan2(movementController.Velocity2D.y * -1, movementController.Velocity2D.x) * Mathf.Rad2Deg - 90, 0);
+        }
+    }
 }
