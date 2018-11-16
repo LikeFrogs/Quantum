52e2a3089d4b26ec1953ccfdde365efe129a8b27
Davis Snider <davissnider42@hotmail.com>
2018-11-16 16:06:08 -0500

    Three Level Demo in place. Start from Level1

--- Assets/Scripts/Combine.cs
+++ Assets/Scripts/Combine.cs
@@ -8,22 +8,10 @@ public enum Characters { World1, World2, Both, SuperTestPosition }
 /// Handles the combining and uncombining of the player characters based on user input.
 /// As well as handling death and respawning
 /// </summary>
-<<<<<<< HEAD
 public class Combine : MonoBehaviour
 {
-    [SerializeField] private Material world1Mat;
-    [SerializeField] private Material world2Mat;
-    [SerializeField] private Material bothWorldMat;
-    [SerializeField] private List<GameObject> world1Phasers;
-    [SerializeField] private List<GameObject> world2Phasers;
-
-    [SerializeField] float distanceToleracne;
-    private bool intersecting;
-    [SerializeField] GameObject mysteriousThirdCylinder;
-
-=======
-public class Combine : MonoBehaviour {
->>>>>>> UI
+    [SerializeField] private string nextLevel;
+    
     [SerializeField] CameraController cameraController;
     private bool introScrolling = true;
 
@@ -53,66 +41,34 @@ public class Combine : MonoBehaviour {
     void Update()
     {
         //kill characters if they are at a certain height
-        if(world1Character.transform.position.y <= -10) {
+        if (world1Character.transform.position.y <= -10)
+        {
             KillCharacter1();
         }
-        if(world2Character.transform.position.y <= -10) {
+        if (world2Character.transform.position.y <= -10)
+        {
             KillCharacter2();
         }
 
 
-<<<<<<< HEAD
-        //reactiveate the inactive character at the position of the character that is currently active
+        //reactive the inactive character at the position of the character that is currently active
         if (Input.GetKeyDown(KeyCode.Alpha3) && IsSingleCharacterInCheckpoint())
         {
-=======
-        //reactive the inactive character at the position of the character that is currently active
-        if (Input.GetKeyDown(KeyCode.Alpha3) && IsSingleCharacterInCheckpoint()) {
->>>>>>> UI
             EnterSuperPosition();
         }
 
         //handle respawning at checkpoints
-        if(AreBothCharactersInDoors()) {
-            LoadCheckpoint();
+        if (AreBothCharactersInDoors())
+        {
+            LoadNextLevel();
         }
 
 
         //update the camera tracking after the exit has been shown
-        if(introScrolling && cameraController.IsInTargetPosition()) {
+        if (introScrolling && cameraController.IsInTargetPosition())
+        {
             cameraController.SetCameraTargets(world1Character.transform, world2Character.transform);
             introScrolling = false;
-        }
-
-        if(AreCharactersOverlapping())
-        {
-            if(!intersecting)
-            {
-                MergeCharacters();
-            }
-
-            intersecting = true;
-        }
-        else
-        {
-            if(intersecting)
-            {
-                UnMerge();
-            }
-
-            intersecting = false;
-        }
-
-        if(intersecting)
-        {
-            SetCharactersOverlapping();
-        }
-        else
-        {
-            mysteriousThirdCylinder.SetActive(false);
-
-            world1Character.GetComponentInChildren<Renderer>().enabled = true;
-            world2Character.GetComponentInChildren<Renderer>().enabled = true;
         }
 
         //uncomment to manually control kill a character
@@ -139,7 +95,7 @@ public class Combine : MonoBehaviour {
     {
         //checks to see if world 1 is the only surviving character
         //and reloads the scene if so
-        if(activeCharacters == Characters.World1)
+        if (activeCharacters == Characters.World1)
         {
             LoadCheckpoint();
         }
@@ -161,7 +117,7 @@ public class Combine : MonoBehaviour {
     {
         //checks to see if world 2 is the oly surviving character
         //and reloads the scene if so
-        if(activeCharacters == Characters.World2)
+        if (activeCharacters == Characters.World2)
         {
             LoadCheckpoint();
         }
@@ -198,7 +154,7 @@ public class Combine : MonoBehaviour {
             world1Character.SetActive(true);
             world1Character.GetComponent<Rigidbody>().velocity = world2Character.GetComponent<Rigidbody>().velocity;
             world1Character.transform.position = world2Character.transform.position;
-            world1Character.transform.rotation = world2Character.transform.rotation;    
+            world1Character.transform.rotation = world2Character.transform.rotation;
         }
     }
 
@@ -212,13 +168,21 @@ public class Combine : MonoBehaviour {
     }
 
     /// <summary>
+    /// Loads the next level
+    /// </summary>
+    private void LoadNextLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nextLevel);
+    }
+
+    /// <summary>
     /// Checks to see if both only a single character is active and if that character is within one of the checkpoints/spawn zones
     /// </summary>
     /// <returns>True if only a single character is active and within a checkpoint</returns>
     private bool IsSingleCharacterInCheckpoint()
     {
         //if both characters are active this is false
-        if(activeCharacters == Characters.Both)
+        if (activeCharacters == Characters.Both)
         {
             return false;
         }
@@ -227,7 +191,7 @@ public class Combine : MonoBehaviour {
         CapsuleCollider collider = ((activeCharacters == Characters.World1) ? world1Character : world2Character).GetComponentInChildren<CapsuleCollider>();
         for (int i = 0; i < checkpoints.Count; i++)
         {
-            if(collider.bounds.Intersects(checkpoints[i].bounds))
+            if (collider.bounds.Intersects(checkpoints[i].bounds))
             {
                 return true;
             }
@@ -242,7 +206,7 @@ public class Combine : MonoBehaviour {
     /// <returns>True if the characters are in their doors</returns>
     private bool AreBothCharactersInDoors()
     {
-        if(activeCharacters != Characters.Both)
+        if (activeCharacters != Characters.Both)
         {
             return false;
         }
@@ -251,72 +215,11 @@ public class Combine : MonoBehaviour {
         CapsuleCollider world1Collider = world1Character.GetComponentInChildren<CapsuleCollider>();
         CapsuleCollider world2Collider = world2Character.GetComponentInChildren<CapsuleCollider>();
 
-        if(world1Collider.bounds.Intersects(world1Door.bounds) && world2Collider.bounds.Intersects(world2Door.bounds))
+        if (world1Collider.bounds.Intersects(world1Door.bounds) && world2Collider.bounds.Intersects(world2Door.bounds))
         {
             return true;
         }
 
         return false;
-    }
-
-    private bool AreCharactersOverlapping()
-    {
-        if(activeCharacters != Characters.Both)
-        {
-            return false;
-        }
-
-        if((world1Character.transform.position - world2Character.transform.position).sqrMagnitude < distanceToleracne)
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-    private void SetCharactersOverlapping()
-    {
-        mysteriousThirdCylinder.transform.position = new Vector3((world1Character.transform.position.x + world2Character.transform.position.x) / 2,
-                                                         (world1Character.transform.position.y + world2Character.transform.position.y) / 2 + 1,
-                                                         (world1Character.transform.position.z + world2Character.transform.position.z) / 2);
-    }
-
-    private void MergeCharacters()
-    {
-        world1Character.transform.position = new Vector3((world1Character.transform.position.x + world2Character.transform.position.x) / 2,
-                                                         (world1Character.transform.position.y + world2Character.transform.position.y) / 2,
-                                                         (world1Character.transform.position.z + world2Character.transform.position.z) / 2);
-        world2Character.transform.position = world1Character.transform.position;
-
-        mysteriousThirdCylinder.SetActive(true);
-        mysteriousThirdCylinder.transform.position = world1Character.transform.position;
-
-        world1Character.GetComponentInChildren<Renderer>().enabled = false;
-        world2Character.GetComponentInChildren<Renderer>().enabled = false;
-
-        for (int i = 0; i < world1Phasers.Count; i++)
-        {
-            world1Phasers[i].layer = 11;
-            world1Phasers[i].GetComponent<Renderer>().material = bothWorldMat;
-        }
-        for (int i = 0; i < world2Phasers.Count; i++)
-        {
-            world2Phasers[i].layer = 11;
-            world2Phasers[i].GetComponent<Renderer>().material = bothWorldMat;
-        }
-    }
-
-    private void UnMerge()
-    {
-        for (int i = 0; i < world1Phasers.Count; i++)
-        {
-            world1Phasers[i].layer = 9;
-            world1Phasers[i].GetComponent<Renderer>().material = world1Mat;
-        }
-        for (int i = 0; i < world2Phasers.Count; i++)
-        {
-            world2Phasers[i].layer = 10;
-            world2Phasers[i].GetComponent<Renderer>().material = world2Mat;
-        }
     }
 }
